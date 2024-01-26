d2c6b048e53479c0f1c41f4493a51505f5687dcc
mizudrakon <mizudrakon@gmail.com>
2024-01-26 10:24:27 +0100

    mostly added comments and small code updates in rational.hpp

--- Lingebra/ratio.hpp
+++ Lingebra/ratio.hpp
@@ -40,62 +40,86 @@ inline std::tuple<int,int,int> lcm(int a, int b){
     return std::make_tuple(a,am,bm);
 }
 
+/*
+    -rational is meant to keep numbers as fractions rather than doubles,
+    but we can't just keep every number as a fraction recursively, and 
+    we need to simplify the fractions wherever possible...
+    -should I leave it with implicite cast to int, or make it explicite?...
+*/
 class rational 
 {
     int nom {0};
     int den {1};
 public:
     rational() = delete;
+    //default c_tor, implicit 0/1, one argument n gives us n/1
     rational(int n = 0, int d = 1):nom(n),den(d){}
+    //copy c_tor
     rational(const rational& rn):nom(rn.nom),den(rn.den){}
+    //move c_tor
     rational(rational&& rn) noexcept : nom(std::move(rn.nom)),den(std::move(rn.den)){}
+    //copy_swap operator, pass by value makes a copy which is returned as the desired new object
     rational& operator=(rational rn)noexcept {
         std::swap(nom, rn.nom);
         std::swap(den, rn.den);
         return *this;
     }
+    //we need member operators to allow *=, /=, %=
     rational& operator*=(const rational& frac){
         nom *= frac.nom;
         den *= frac.den;
+        reduce();
         return *this;
     }
     rational& operator/=(const rational& frac){
         nom /= frac.nom;
         den /= frac.den;
+        reduce();
         return *this;
     }
     rational& operator%=(rational frac){
-
-        reduce();
-        frac.reduce();
-        auto [com_den, m, rm] = lcm(den,frac.den);
-        rational result((nom*m) % (frac.nom*rm),com_den);
-        std::swap(*this,result);
+    //I want to allow remainder from fraction division too
+        reduce(); //our initial fraction will be switched for the result, so we can just reduce it
+        frac.reduce(); //frac is a copy of some rational argument which itself doesn't get reduced
+        auto [com_den, m, rm] = lcm(den,frac.den); //get lcm and multipliers for the denominators
+        //result should be: (nom*m % frac.nom*rm)/lcm
+        //we can switch them with the original fraction data with modified parameter data
+        frac.set_den(com_den);
+        frac.set_nom((nom*m)%(frac.nom*rm));
+        std::swap(*this,frac);
         return *this;
     }
 
+    //d_tor doesn't really have anything special to do
     ~rational(){}
 
+    //getters and setters for the two int numbers
     int nomin() const { return nom;}
     void set_nom(int n) { nom = n; }
     int denomin() const { return den; }
     void set_den(int d) { den = d; }
+    //functions to get the number as a floating point rather than a fraction
     double dbl() const { return static_cast<double>(nom) / static_cast<double>(den);}
     float flt() const { return static_cast<float>(nom) / static_cast<float>(den);}
+    
+    //function for reducing the fraction
     void reduce();
+
+    //getting it as string for printing
     std::string str() const{
         
         return std::to_string(nom) + "/" + std::to_string(den);
     }
 };
 
+//just divide everything with greatest common denominator, which could be one therefore no change
 inline void rational::reduce(){
     int c = gcd(nom,den);
     nom /= c;
     den /= c;
 }
 
-
+//spaceship operator for comparison
 inline auto operator<=>(const rational& lhs, const rational& rhs){
     if (lhs.denomin() == rhs.denomin()) return lhs.nomin() <=> rhs.nomin();
     auto a = lhs, b = rhs;
@@ -158,6 +182,7 @@ inline auto operator- (rational lhs, rational rhs){
     return result;
 };
 
+//for negation with -
 inline auto operator-(rational frac){
     return 0-frac;
 }
@@ -171,7 +196,10 @@ struct std::formatter<rational> : std::formatter<std::string> {
     }
 };
 
-//OK, this would be a lot of work, so I need to check if I need to do it
+/*
+    exp_num is meant to keep numbers as possibly primes with exponent for use in fractions
+    I mostly want them to keep irrational numbers like sqr(2) 
+*/
 class exp_num
 {
     int base {0};
