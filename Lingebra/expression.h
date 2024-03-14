8692367f2d3f3e54493cb1b0f912cd81cac5f362
mizudrakon <mizudrakon@gmail.com>
2024-03-14 21:47:29 +0100

    preparing expression, need to decide on exp_num

--- Lingebra/expression.h
+++ Lingebra/expression.h
@@ -4,6 +4,22 @@
 #include "ratio.h"
 #include "exp_num.h"
 
+/* Expression is a binary tree
+ * nodes are either values or operations
+ * some operations can be functions
+*/
+
+template <Arithmetic T>
+struct value_node 
+{
+    T value;
+};
+
+struct op_node 
+{
+
+};
+
 class expression
 {
 
