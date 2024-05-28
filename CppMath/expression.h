bb8e4b6e02654808baf8695fa3108563d21dd82d
mizudrakon <mizudrakon@gmail.com>
2024-05-28 17:27:16 +0200

    expression print basics, preparing operations

--- CppMath/expression.h
+++ CppMath/expression.h
@@ -11,15 +11,16 @@ constexpr void print(const std::string_view text, auto&&... args){
     fputs(std::vformat(text,std::make_format_args(args...)).c_str(), stdout);
 }
 
-using std::make_unique, std::unique_ptr;
+using std::make_unique, std::unique_ptr, std::move;
+using std::string;
 
 enum class Op {
     plus,
     minus,
     mult,
     div,
-    pow,
-    root,
+    //pow,
+    //root,
     val
 };
 
@@ -41,6 +42,7 @@ public:
     virtual bool operator==(const node& nd) const = 0;
     virtual node& operator+(const node& nd) = 0;
     virtual node& operator-(const node& nd) = 0;
+    virtual node& operator*(const node& nd) = 0;
 
 };
 
@@ -50,13 +52,14 @@ class value_node:public node
 public:
     value_node(){ print("default value node ctor\n");}
     value_node(int value):val(value){ print("value node int ctor\n");}
-    value_node(const value_node& vn):val(vn.val){ print("value node copy ctor\n");}
-    value_node(value_node&& vn):val(std::move(vn.val)){ print("value node move ctor\n");}
+    value_node(const node& vn):val(static_cast<value_node>(vn).val){ print("value node copy ctor\n");}
+    value_node(node&& vn):val(move(static_cast<value_node>(vn).val)){ print("value node move ctor\n");}
     ~value_node(){
         print("value node dest\n");
     } 
 
-    std::string str() const override { return "{}"; }
+    std::string str() const override { return std::string("{"+std::to_string(val)+"}"); }
+    //getters:
     int getVal() const override {return val;}
     Op getOp() const override { 
         return Op::val;
@@ -85,6 +88,10 @@ public:
         val -= nd.getVal();
         return *this;
     }
+    node& operator*(const node& nd) override {
+        val *= nd.getVal();
+        return *this;
+    }
 
 };
 
@@ -95,18 +102,38 @@ class op_node:public node
     unique_ptr<node> left;
     unique_ptr<node> right;
 public:
-    op_node() = delete;
+    //ctors
+    op_node() = delete;//no default, currently don't know what to do with it
     op_node(Op o):op(o){}
+    op_node(const node& opn):op(static_cast<op_node>(opn).op){
+        print("op_node copy ctor\n");
+        if (left->getOp() == Op::val)
+            left = make_unique<value_node>(*opn.getLeft());
+        else left = make_unique<op_node>(*opn.getLeft());
+        if (left->getOp() == Op::val)
+            right = make_unique<value_node>(*opn.getRight());
+        else right = make_unique<op_node>(*opn.getRight());
+        //this should work recursively
+    }
+    op_node(node&& opn):op(move(static_cast<op_node>(opn).op)),left(move(static_cast<op_node>(opn).left)),
+    right(move(static_cast<op_node>(opn).right))
+    {
+        print("op_node move ctor\n");
+    }
     ~op_node(){
         print("op_node dest\n");
     }
     std::string str() const override;
+    
+    //getters:
     int getVal() const override { return 0; }//there is no value
     Op getOp() const override { return op; }
 
     //return observers:
     node* getLeft() const override {return left.get();}
     node* getRight() const override {return right.get();}
+    
+    //setters:
     void setLeft(unique_ptr<node> nd) override {
         left = std::move(nd);
     }
@@ -124,6 +151,9 @@ public:
         return *this;
     }
     node& operator-(const node& nd) override {
+        return *this;
+    }
+    node& operator*(const node& nd) override {
         return *this;
     }
 };
@@ -148,7 +178,7 @@ inline std::string op_node::str() const
     case Op::div:
         return "/";
         break;
-
+    /*
     case Op::pow:
         return "^";
         break;
@@ -156,6 +186,7 @@ inline std::string op_node::str() const
     case Op::root:
         return "^1/";
         break;
+    */
     default:
         break;
     }
@@ -166,10 +197,82 @@ class expression
 {
     unique_ptr<node> head;
 public:
+    ~expression() = default;//unique pointers should take care of the tree structure
     expression(): head(make_unique<value_node>(0)){ print("default expression ctor\n");}
     expression(int val): head(make_unique<value_node>(val)){print("expression int ctor\n");}
     expression(expression&& exs): head(std::move(exs.head)){print("expression move ctor\n");}
-    //copy constructor and assignemnt will need to traverse the tree
-    ~expression() = default;//unique pointers should take care of the tree structure
-    
+    expression(const expression& rhs){
+        auto p_rhs = rhs.head.get();
+        //copy constructors should work recursively, so it's just about finding out what the head is
+        if (p_rhs->getOp() == Op::val)
+            head = make_unique<value_node>(*p_rhs);
+            //val_node doesn't actually have children
+        else
+            head = make_unique<op_node>(*p_rhs);
+    }
+
+    node* getHead() const{
+        return head.get();
+    }
+
+    string subtree_str(const node* nd) const{
+        if (nd->getOp() == Op::val)
+            return nd->str();
+        return subtree_str(nd->getLeft()) + nd->str() + subtree_str(nd->getRight());
+    }
+
+    string str() const{
+        return subtree_str(head.get());
+    }
+
+    auto operator==(const expression& rhs) const{
+        return head.get() <=> rhs.getHead();
+    }
+    auto& operator+=(const expression& rhs){
+        return *this;
+    }
+    auto& operator++(){
+        return *this;
+    }
+    auto& operator++(int){
+        return *this;
+    }
+    auto& operator-=(const expression& rhs){
+        return *this;
+    }
+    auto& operator--(){
+        return *this;
+    }
+    auto& operator--(int){
+        return *this;
+    }
+    auto& operator*=(const expression& rhs){
+        return *this;
+    }
+    auto& operator/=(const expression& rhs){
+        return *this;
+    }
 };
+
+auto operator<=>(const expression& lhs, const expression& rhs){
+    return lhs.getHead()<=>rhs.getHead();
+}
+auto operator==(const expression& lhs, const expression& rhs){
+    return lhs.getHead()==rhs.getHead();
+}
+auto operator+(expression lhs, const expression& rhs)
+{
+    return lhs;
+}
+auto operator-(expression lhs, const expression& rhs)
+{
+    return lhs;
+}
+auto operator*(expression lhs, const expression& rhs)
+{
+    return lhs;
+}
+auto operator/(expression lhs, const expression& rhs)
+{
+    return lhs;
+}
--- CppMath/tests/testExpress.cpp
+++ CppMath/tests/testExpress.cpp
@@ -2,9 +2,20 @@
 
 int main(void)
 {
+   print("testing value nodes:\n");
    value_node vn(0);
    if (vn.getOp() == Op::val)
       print("it is working\n");
       print("{}",vn.str());
       print("\n");
+   
+   print("testing operation nodes:\n");
+   op_node o_plus(Op::plus);
+   print("operation is {}\n", o_plus.str());
+
+
+   print("testing expression:\n");
+   expression defexp{};
+   expression eone{1};
+   print("default expression: {}, eone: {}\n", defexp.str(), eone.str());
 }
